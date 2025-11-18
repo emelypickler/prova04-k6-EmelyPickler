3c11bcc14789ca754a4b35f21c0dd2baff755d62
emelypickler <emelypickler04@gmail.com>
2025-11-18 19:50:51 -0300

    .

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,19 +4,20 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+export const getRequestDuration = new Trend('get_request_duration', true);
+export const rateStatusOk = new Rate('rate_status_ok');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_duration: ['p(90)<6800'],
+    get_request_duration: ['p(90)<6800'],
+    http_req_failed: ['rate<0.25'],
+    rate_status_ok: ['rate>0.75']
   },
   stages: [
-    { duration: '10s', target: 2 },
-    { duration: '10s', target: 4 },
-    { duration: '10s', target: 6 }
+    { duration: '30s', target: 7 },
+    { duration: '2m', target: 92 },
+    { duration: '1m', target: 92 }
   ]
 };
 
@@ -28,23 +29,22 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const url = 'https://dummyjson.com/products/1';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  const res = http.get(url);
 
-  const OK = 200;
-
-  const res = http.get(`${baseUrl}`, params);
-
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status === OK);
+  getRequestDuration.add(res.timings.duration);
+  rateStatusOk.add(res.status === 200);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'Status 200 OK': () => res.status === 200,
+    'Response tem campo title': () => {
+      try {
+        const body = JSON.parse(res.body);
+        return Boolean(body.title);
+      } catch (e) {
+        return false;
+      }
+    }
   });
 }
